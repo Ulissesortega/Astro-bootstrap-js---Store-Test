aa19b2d7cbd56360803f7054e1e5468286074779
Ulises_Ortega <ulisses.ortega@hotmail.com>
2024-10-17 09:01:04 -0700

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const mainImage = document.getElementById('mainImage');
+
+const thumbnails = document.querySelectorAll('.thumbnail');
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('mouseover', function () {
+        // When the mouse is over a thumbnail, change the src of the main image
+        mainImage.src = this.src;
+    });
+});
+
